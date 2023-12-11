92042c90723b0006b6d28e23a3af101be2fc5fa3
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-11 13:33:23 +0100

    Update siddump.c

--- siddump.c
+++ siddump.c
@@ -1182,20 +1182,3 @@ unsigned short readword(FILE *f)
   fread(&res, 2, 1, f);
   return (res[0] << 8) | res[1];
 }
-
-
-unsigned char readbyte(FILE *f)
-{
-  unsigned char res;
-
-  fread(&res, 1, 1, f);
-  return res;
-}
-
-unsigned short readword(FILE *f)
-{
-  unsigned char res[2];
-
-  fread(&res, 2, 1, f);
-  return (res[0] << 8) | res[1];
-}
